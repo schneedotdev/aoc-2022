30b543bc552955bed7ff5a0a934447a1282849c4
brianschnee <brian.a.schnee@gmail.com>
2022-12-01 14:10:40 -0500

    day-01 part b solution

---
+++ day-01/b.ts
@@ -0,0 +1,14 @@
+const input = await Deno.readTextFile("./input.txt")
+const inventories = input.split('\n\n')
+
+const elves = inventories.map(inventory => {
+    return inventory
+        .split('\n')
+        .reduce((calories, meal) => calories += parseInt(meal, 10), 0)
+})
+
+elves.sort((a, b) => a - b)
+
+const sum = (elves.at(-1) || 0) + (elves.at(-2) || 0) + (elves.at(-3) || 0)
+
+console.log("sum of the top 3 elves calories: ", sum)
