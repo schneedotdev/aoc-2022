9a89eb4b12a7667b95d9b38600d6a6575071712d
brianschnee <brian.a.schnee@gmail.com>
2022-12-03 00:58:02 -0500

    day-03 part a refactor

--- day-03/typescript/a.ts
+++ day-03/typescript/a.ts
@@ -4,19 +4,18 @@ const lines = input.split('\n')
 const priority = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 const sum = lines.reduce((sum, line) => {
-    const halfLen = Math.ceil(line.length / 2);
+    const halfLen = Math.ceil(line.length / 2)
     const firstHalf = line.substring(0, halfLen)
     const secondHalf = line.substring(halfLen)
-
-    const commonalities: string[] = []
     
     for(const char of firstHalf) {
-        if(!commonalities.includes(char) && secondHalf.includes(char)) {
-            commonalities.push(char)
+        if(secondHalf.includes(char)) {
+            sum += priority.indexOf(char) + 1
+            break;
         }
     }
 
-    return sum + commonalities.reduce((sum, char) => sum + priority.indexOf(char) + 1, 0)
+    return sum
 }, 0)
 
 console.log(sum) // answer: 7553
